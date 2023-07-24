7c98e2d2f7428a0ab5c3cbba281047661185e25e
danilolimacomp@gmail.com <djkcond@gmail.com>
2023-07-24 17:51:27 -0300

    criando banco de dados e imprimindo temperatura e umidade

--- page/app.py
+++ page/app.py
@@ -9,14 +9,12 @@ import Adafruit_DHT as DHT
 import RPi.GPIO as GPIO
 import time
 
-def hora():
-    now = datetime.datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    hour = now.hour
-    minute = now.minute
-    return (year, month, day, hour, minute)
+now = datetime.datetime.now()
+ano = now.year
+mes = now.month
+dia = now.day
+hora = now.hour
+minuto = now.minute
 
 
 
@@ -26,42 +24,37 @@ pino_sensor = 25
 umid, temp = DHT.read_retry(sensor, pino_sensor)
 
 
-def salvando(umidade):
-    # Crie uma conexão com o banco de dados
-    conn = sqlite3.connect("dados.sql")
+# Crie uma conexão com o banco de dados
+conn = sqlite3.connect("dados.sql")
 
-    # Crie um cursor
-    c = conn.cursor()
+# Crie um cursor
+c = conn.cursor()
 
-    # Insira os dados na tabela
-    c.execute("INSERT INTO dados (created, umidade, temperatura, pressao, CO) VALUES (?, ?, ?, ?, ?)", (hora(), umidade, 0, 0, 0))
+# Insira os dados na tabela
+c.execute("INSERT INTO dados (ano, mes, dia, hora, minuto, umidade, temperatura, pressao, CO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (ano, mes, dia, hora, minuto, umid, temp, 0, 0))
 
-    # Salve as alterações
-    conn.commit()
+# Salve as alterações
+conn.commit()
 
-    # Feche a conexão
-    conn.close()
+# Feche a conexão
+conn.close()
 
-def imprimindo():
-    # Crie uma conexão com o banco de dados
-    conn = sqlite3.connect("meu_banco_de_dados.sqlite")
+# Crie uma conexão com o banco de dados
+conn = sqlite3.connect("meu_banco_de_dados.sqlite")
 
-    # Crie um cursor
-    c = conn.cursor()
+# Crie um cursor
+c = conn.cursor()
 
-    # Execute uma consulta SQL
-    c.execute("SELECT * FROM tabela")
+# Execute uma consulta SQL
+c.execute("SELECT * FROM tabela")
 
-    # Obtenha os resultados da consulta
-    rows = c.fetchall()
+# Obtenha os resultados da consulta
+rows = c.fetchall()
 
-    # Imprima os resultados no prompt
-    for row in rows:
-        print(row)
+# Imprima os resultados no prompt
+for row in rows:
+    print(row)
 
-    # Feche a conexão
-    conn.close()
+# Feche a conexão
+conn.close()
 
-
-salvando(umid)
-imprimindo()
